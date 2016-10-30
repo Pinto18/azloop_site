7f3b87648abbfdccf73ec3e94d7dbe8de8026458
Pinto18 <pinto18.nd@gmail.com>
2016-10-30 12:26:23 -0700

    Created a team member model

---
+++ db/migrate/20161030192540_create_team_members.rb
@@ -0,0 +1,11 @@
+class CreateTeamMembers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :team_members do |t|
+      t.string :name
+      t.string :bio
+      t.string :position
+
+      t.timestamps
+    end
+  end
+end
