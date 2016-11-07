0e8da43d864083146a8f4bdd0418c07a75a0f46d
Pinto18 <pinto18.nd@gmail.com>
2016-11-06 22:03:34 -0700

    Updating Assets

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
     
-    test "layout links" do
+    test "layout links_home" do
         get root_path
         assert_template 'static_pages/home'
         assert_select "a[href=?]", root_path, count: 2
@@ -12,6 +12,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
         assert_select "a[href=?]", sponsors_path
         assert_select "a[href=?]", news_path
         assert_select "a[href=?]", join_path
+        assert_select "a[href=?]", contact_path
+    end
+
+    test "layout links_team" do
+        get team_path
+        assert_template 'static_pages/team'
+        assert_select "a[href=?]", root_path, count: 2
+        assert_select "a[href=?]", design_path
+        assert_select "a[href=?]", about_path
+        assert_select "a[href=?]", team_path
+        assert_select "a[href=?]", sponsors_path, count: 2
+        assert_select "a[href=?]", news_path
+        assert_select "a[href=?]", join_path, count: 2
         assert_select "a[href=?]", contact_path
     end
 end
